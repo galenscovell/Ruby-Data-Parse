eb10f7a31285021da928fb7890d736d9cab4dcc7
Galen Scovell <galen.scovell@gmail.com>
2014-11-06 20:21:35 -0800

    Toying with CSV Module
    Hoping to get this like the back of my hand in short order.

--- CSV_parse.rb
+++ CSV_parse.rb
@@ -2,17 +2,36 @@
 
 require 'csv'
 
-
 puts "Data Parser Initialized."
 
 
-contents = CSV.foreach('event_attendees_full.csv') do |row|
-    puts row.inspect
+def save_output(file, searched, term)
+    Dir.mkdir("searches") unless Dir.exists?("searches")
+    filename = "searches/#{term}.txt"
+
+    File.open(filename, 'w') do |file|
+        file.puts searched
+    end
+    puts "Output saved to /searches as #{term}.txt."
 end
 
-printf "Search term: "
-search_term = gets.chomp
 
+def search_data(file, term)
+    searched_data = []
+    CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
+        if row[:first_name] == term
+            searched_data << row
+        end
+    end
+    save_output(file, searched_data, term)
+end
+
+contents = "event_attendees_full.csv"
+
+printf "Search term: "
+search_key = gets.chomp
+
+search_data(contents, search_key)
 
 
 
