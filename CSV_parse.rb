82806a7d6da678eb7041823aaee11f023fe410d1
Galen Scovell <galen.scovell@gmail.com>
2014-11-07 11:49:37 -0800

    Program loop, refine code
    Program loops without reset. Save searches under filename with date and
    search term. Going to begin refining the returned data, as it defaults
    to a bit of a mess right now.

--- CSV_parse.rb
+++ CSV_parse.rb
@@ -5,33 +5,44 @@ require 'csv'
 puts "Data Parser Initialized."
 
 
-def save_output(file, searched, term)
+def save_output(searched, term)
+    current_time = Time.now.strftime('%m_%d_%y')
     Dir.mkdir("searches") unless Dir.exists?("searches")
-    filename = "searches/#{term}.txt"
+    filename = "searches/#{current_time}_#{term}.txt"
 
     File.open(filename, 'w') do |file|
         file.puts searched
     end
-    puts "Output saved to /searches as #{term}.txt."
+
+    puts "Output saved to /searches as '#{current_time}_#{term}.txt'."
 end
 
 
 def search_data(file, term)
     searched_data = []
     CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
+        id_num = row[0]
         if row[:first_name] == term
             searched_data << row
+            searched_data << [""]
         end
     end
-    save_output(file, searched_data, term)
+    save_output(searched_data, term)
 end
 
 contents = "event_attendees_full.csv"
 
-printf "Search term: "
-search_key = gets.chomp
-
-search_data(contents, search_key)
+end_script = false
+until end_script
+    printf "Search term ('quit' to end): "
+    search_key = gets.chomp    
+    if search_key != 'quit'
+        search_data(contents, search_key)
+    else
+        end_script = true
+    end
+end
+puts 'Closing Data Parser.'
 
 
 
