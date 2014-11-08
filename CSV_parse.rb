b420f8c9e3a743acf69613fd2064ca5271ed44bd
Galen Scovell <galen.scovell@gmail.com>
2014-11-08 11:55:32 -0800

    Correct Indentation
    Didn't even notice that Sublime Text forgot my 2 space indentation when
    working with Ruby...

--- CSV_parse.rb
+++ CSV_parse.rb
@@ -6,41 +6,41 @@ puts "Data Parser Initialized."
 
 
 def save_output(searched, term)
-    current_time = Time.now.strftime('%m_%d_%y')
-    Dir.mkdir("searches") unless Dir.exists?("searches")
-    filename = "searches/#{current_time}_#{term}.txt"
+  current_time = Time.now.strftime('%m_%d_%y')
+  Dir.mkdir("searches") unless Dir.exists?("searches")
+  filename = "searches/#{current_time}_#{term}.txt"
 
-    File.open(filename, 'w') do |file|
-        file.puts searched
-    end
+  File.open(filename, 'w') do |file|
+    file.puts searched
+  end
 
-    puts "Output saved to /searches as '#{current_time}_#{term}.txt'."
+  puts "Output saved to /searches as '#{current_time}_#{term}.txt'."
 end
 
 
 def search_data(file, term)
-    searched_data = []
-    CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
-        id_num = row[0]
-        if row[:first_name] == term
-            searched_data << row
-            searched_data << [""]
-        end
+  searched_data = []
+  CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
+    id_num = row[0]
+    if row[:first_name] == term
+      searched_data << row
+      searched_data << [""]
     end
-    save_output(searched_data, term)
+  end
+  save_output(searched_data, term)
 end
 
 contents = "event_attendees_full.csv"
 
 end_script = false
 until end_script
-    printf "Search term ('quit' to end): "
-    search_key = gets.chomp    
-    if search_key != 'quit'
-        search_data(contents, search_key)
-    else
-        end_script = true
-    end
+  printf "Search term ('quit' to end): "
+  search_key = gets.chomp    
+  if search_key != 'quit'
+    search_data(contents, search_key)
+  else
+    end_script = true
+  end
 end
 puts 'Closing Data Parser.'
 
